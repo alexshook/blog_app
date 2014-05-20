fbe2c0a22487d4aed1841ddf042babaea9a1bca2
alexshook <alexandra.shook@gmail.com>
2014-05-19 20:59:44 -0400

    rspec capybara feature and controller tests

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe PostsController do
+  describe 'GET index' do
+    it 'is successful' do
+      get :index
+      expect(response).to be_success
+    end
+
+    it 'assigns posts' do
+      posts = []
+      10.times do
+        posts << FactoryGirl.create(:post)
+      end
+      get :index
+      expect(assigns(:posts)).to eq posts
+    end
+  end
+
+  describe 'GET show' do
+    before :each do
+      @post = FactoryGirl.create(:post)
+      get :show, id: @post.id
+    end
+
+    it 'is sucessful' do
+      expect(response).to be_success
+    end
+
+    it 'assigns post' do
+      expect(assigns(:post)).to eq @post
+    end
+  end
+
+describe 'GET new' do
+    before :each do
+      get :new
+    end
+
+    it 'is sucessful' do
+      expect(response).to be_success
+    end
+
+    it 'assigns post' do
+      expect(assigns(:post)).is_a? Post
+    end
+  end
+
+end
---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature 'Posts' do
+  scenario 'Shows all posts' do
+    posts = []
+    10.times do
+      posts << FactoryGirl.create(:post)
+    end
+    visit root_path
+    # save_and_open_page
+    posts.each do |p|
+      expect(page).to have_content p.title
+    end
+  end
+
+  scenario 'Shows individual post' do
+    post = FactoryGirl.create(:post)
+    visit post_path id: post.id
+    expect(page).to have_content post.title
+    expect(page).to have_content post.body
+  end
+
+  scenario 'Creates a new post' do
+    post_attributes = FactoryGirl.attributes_for(:post)
+    visit new_post_path
+    save_and_open_page
+    fill_in 'Title', with: post_attributes[:title]
+    fill_in 'Body', with: post_attributes[:body]
+    click_button 'Create Post'
+    expect(page).to have_content post_attributes[:title]
+    expect(page).to have_content post_attributes[:body]
+  end
+
+end
